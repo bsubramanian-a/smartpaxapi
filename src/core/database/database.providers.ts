ca4d42cb6b2e5f72b08f380dc50f23d8b9b5859e
Rajavel Balasubramanian <bsubramanian.a@gmail.com>
2023-01-10 20:50:20 +0530

    final changes in db schema

--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -5,6 +5,27 @@ import { Item } from 'src/modules/items/item.entity';
 import { User } from 'src/modules/users/user.entity';
 import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
 import { databaseConfig } from './database.config';
+import { UPermission } from 'src/modules/permission/permission.entity';
+import { Benchmark } from 'src/modules/benchmarks/benchmark.entity';
+import { ChartCategory } from 'src/modules/chartcategories/chartcatagory.entity';
+import { Chart } from 'src/modules/charts/chart.entity';
+import { ChartSettingGroup } from 'src/modules/chartsettinggroups/chartsettinggroup.entity';
+import { ChartSetting } from 'src/modules/chartsettings/chartsetting.entity';
+import { ChartType } from 'src/modules/charttypes/charttype.entity';
+import { GColumn } from 'src/modules/columns/column.entity';
+import { CDropdown } from 'src/modules/columndropdowns/columndropdown.entity';
+import { CPeople } from 'src/modules/columnpeople/columnpeople.entity';
+import { CStatus } from 'src/modules/columnstatuses/columnstatus.entity';
+import { CValue } from 'src/modules/columnvalues/columnvalue.entity';
+import { DColumnDropdown } from 'src/modules/defaultcolumndropdowns/defaultcolumndropdown.entity';
+import { DColumnPeople } from 'src/modules/defaultcolumnpeople/defaultcolumnpeople.entity';
+import { DColumnStatus } from 'src/modules/defaultcolumnstatus/defaultcolumnstatus.entity';
+import { DColumn } from 'src/modules/defaultcolumns/defaultcolumn.entity';
+import { Filter } from 'src/modules/filter/filter.entity';
+import { Group } from 'src/modules/groups/group.entity';
+import { GRow } from 'src/modules/rows/row.entity';
+import { Sort } from 'src/modules/sort/sort.entity';
+import { Dropdown } from 'src/modules/dropdown/dropdown.entity';
 
 export const databaseProviders = [{
     provide: SEQUELIZE,
@@ -29,7 +50,33 @@ export const databaseProviders = [{
             repositoryMode:true
             //dialect: config.dialect
         });
-        sequelize.addModels([User, Invitation, InvitationItem, Item]);
+        sequelize.addModels([
+         User, 
+         Invitation, 
+         InvitationItem, 
+         Item, 
+         UPermission, 
+         Benchmark, 
+         ChartCategory, 
+         Chart, 
+         ChartSettingGroup,
+         ChartSetting,
+         ChartType,
+         GColumn,
+         Dropdown,
+         CDropdown,
+         CPeople,
+         CStatus,
+         CValue,
+         DColumnDropdown,
+         DColumnPeople,
+         DColumnStatus,
+         DColumn,
+         Filter,
+         Group,
+         GRow,
+         Sort
+      ]);
         await sequelize.sync();
         return sequelize;
     },
