dc463e3727b7e7878b0e6ea133f4a95fd348dd3c
Rajavel Balasubramanian <bsubramanian.a@gmail.com>
2023-01-11 16:57:35 +0530

    menuitems

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
-    @Get(':id')
-    menuItems(@Param('id') id:string): string {
-        return id;
+    @Get('menuItems/:id')
+    menuItems(@Param('id') id: string, @Res() res: Response) {
+        
+        let responseJSON = { "id": id, "workspace": [], "dashboard": [] }
+        res.status(200).send(responseJSON);
     }
 }
